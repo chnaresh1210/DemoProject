0a2db20dc14d5643cac09e9dc5894c178a577e8c
Naresh Kumar <chnaresh1210@gmail.com>
2019-08-26 23:12:29 +0530

    paati

---
+++ SampleProject/src/Raja_Paati.java
@@ -0,0 +1,10 @@
+
+public class Raja_Paati {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("pace institute of technology and scicens");
+	}
+
+}
